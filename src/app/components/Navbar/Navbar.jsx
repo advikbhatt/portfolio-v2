ce78b60c07510e1666a9b04e722d2518f4f0e0bf
Advik <advikbhatt24@gmail.com>
2025-06-27 21:07:06 +0530

    navbar issue fixed

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -4,14 +4,14 @@ import { TbMail, TbBrandLinkedin, TbBrandGithub } from 'react-icons/tb';
 export default function Navbar() {
   return (
     <header className="navbar">
-      <div className="navbar__location">Dehradun, IN</div>
+      {/* <div className="navbar__location">Dehradun, IN</div> */}
 
-      <div className="navbar__logo">ADVIK BHATT</div>
-<div className="navbar__socials">
+      {/* <div className="navbar__logo">ADVIK BHATT</div> */}
+{/* <div className="navbar__socials">
   <a href="mailto:advikbhatt24@gmail.com"><TbMail /></a>
   <a href="https://www.linkedin.com/in/advikbhatt/"><TbBrandLinkedin /></a>
   <a href="https://github.com/advikbhatt"><TbBrandGithub /></a>
-</div>
+</div> */}
 
     </header>
   );
